96c33167e5550e1528a8d7901e8c4f34d003da45
bracinho2 <bracinho2@hotmail.com>
2022-08-26 07:56:49 -0300

    Início da AppBar

---
+++ lib/app/core/styles/app_text_styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle appBarTitle = const TextStyle(
+    fontSize: 12,
+    fontFamily: 'Roboto',
+    color: Colors.grey,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle appBarSubtitle = const TextStyle(
+    fontSize: 20,
+    fontFamily: 'Roboto',
+    color: Colors.black,
+    fontWeight: FontWeight.w900,
+  );
+}
+
+
+//font Roboto
