36a3aca5012f4febbe20eb7dc1ee3e298fa84d9b
bracinho2 <bracinho2@hotmail.com>
2022-09-05 07:57:07 -0300

    Atualizações do final de semana

--- lib/app/modules/home_page/home_page.dart
+++ lib/app/modules/home_page/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desafio01_interface/app/core/styles/app_colors.dart';
+import 'package:flutter_desafio01_interface/app/core/app_resposivity.dart';
 import 'package:flutter_desafio01_interface/app/modules/home_page/widgets/app_bar_home_page_widget.dart';
 import 'package:flutter_desafio01_interface/app/modules/home_page/widgets/custom_sliver_horizontal_bar.dart';
 import 'package:flutter_desafio01_interface/app/modules/home_page/widgets/custom_sliver_vertical_bar.dart';
@@ -9,26 +9,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQueryData = MediaQuery.of(context);
     return SafeArea(
       child: Scaffold(
         appBar: const AppBarHomePageWidget(
           title: 'Localização',
           subTitle: 'Paraná, Sulina PR',
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              expandedHeight: Resposivity.automatic(200, mediaQueryData),
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset(
+                  'assets/images/gaita.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            color: AppColors.cinzaFraco,
-          ),
-          child: const CustomScrollView(
-            slivers: <Widget>[
-              CustomSliverHorizontalBar(),
-              //CustomSliverVerticalBar(),
-            ],
-          ),
+            const CustomSliverHorizontalBar(),
+            const CustomSliverVerticalBar(),
+          ],
         ),
       ),
     );
